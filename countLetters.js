fb6ec3250296aa0968de1aee21c075537ca9383e
Dante Cross <dantecross@outlook.com>
2020-10-28 22:57:40 +0000

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+ 
+  if (actual === expected) {
+    console.log(`🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const numOfLetters = {};
+
+  for (const letters of string) {
+    if (letters !== ' ') {
+      if (numOfLetters[letters]) {
+      numOfLetters[letters] += 1;
+    } else {
+      numOfLetters[letters] = 1;
+    }
+  } 
+} return numOfLetters;
+};
+
+console.log(countLetters("lighthouse in the house"));
+assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2,u: 2, s: 2, e: 3, n: 1});
